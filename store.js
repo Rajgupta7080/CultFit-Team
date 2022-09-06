fb5c73a7750721a6b6d08cefdb9ad2abc1eef680
komal kumar <er.kumarkomal@gmail.com>
2022-09-06 20:16:11 +0530

    show product on page

--- store.js
+++ store.js
@@ -1,9 +1,18 @@
-function get() {
-  console.log("mememememem");
-}
-const fun = async () => {
-  const data = await fetch(`http://localhost:8004/bestseller`);
-  const data2 = await data.json();
-  console.log(data2);
-};
-fun();
+import { getdata, navbar, showdata } from "../utility.js";
+
+document.getElementById("navbar").innerHTML = navbar;
+// function get() {
+//   console.log("mememememem");
+// }
+//
+// fun();
+const bestseller = await getdata("bestseller");
+showdata(bestseller, "BESTSELLERS", "LOVED BY CULT MEMBERS");
+const Just_launched = await getdata("Just_launched");
+showdata(Just_launched, "JUST LAUNCHED", "NEW ARRIVALS");
+const Trade_mills = await getdata("Trade_mills");
+showdata(Trade_mills, "TREADMILLS", "LOVED BY CULT MEMBERS");
+const SPINBIKES = await getdata("SPINBIKES");
+showdata(SPINBIKES, "SPINBIKES", "LOVED BY CULT MEMBERS");
+const Cycles = await getdata("Cycles");
+showdata(Cycles, "CYCLES", "LOVED BY CULT MEMBERS");
