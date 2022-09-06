ef43dd572acdf54751e50507bec940d47c24871c
Suraj <surajybtg@gmail.com>
2022-09-06 12:20:10 +0530

    Fitness page updated

---
+++ Fitness/scripts/fitness.js
@@ -0,0 +1,157 @@
+document.getElementById("flex1").addEventListener("click", p1);
+document.getElementById("flex2").addEventListener("click", p2);
+document.getElementById("flex3").addEventListener("click", p3);
+document.getElementById("flex4").addEventListener("click", p4);
+document.getElementById("flex5").addEventListener("click", p5);
+document.getElementById("flex6").addEventListener("click", p6);
+document.getElementById("flex7").addEventListener("click", p7);
+document.getElementById("flex8").addEventListener("click", p8);
+document.getElementById("flex9").addEventListener("click", p9);
+document.getElementById("flex10").addEventListener("click", p10);
+document.getElementById("flex11").addEventListener("click", p11);
+document.getElementById("flex12").addEventListener("click", p12);
+
+var clickforp1 = 0;
+var clickforp2 = 0;
+var clickforp3 = 0;
+var clickforp4 = 0;
+var clickforp5 = 0;
+var clickforp6 = 0;
+var clickforp7 = 0;
+var clickforp8 = 0;
+var clickforp9 = 0;
+var clickforp10 = 0;
+var clickforp11 = 0;
+var clickforp12 = 0;
+
+function p1() {
+  if (clickforp1 == 0) {
+    document.getElementById("p1").innerText =
+      "At Cult, we make fitness fun and easy. We have best-in-class trainers & offer group workouts ranging from yoga to Boxing. Our workouts can be done both at a cult center and at home with the help of do it yourself (DIY) workout videos. Cult.fit uses the best in technology to give you a world-class experience. You can book classes, follow workout videos - all with the click of a button from the cult.fit app or website. ";
+    clickforp1++;
+  } else {
+    document.getElementById("p1").innerText = "";
+    clickforp1--;
+  }
+}
+
+function p2() {
+  if (clickforp2 == 0) {
+    document.getElementById("p2").innerText =
+      "At cult centers, you’ll find a wide variety of workout formats, such as - Boxing, Dance Fitness, Yoga, S&C, and HRX. These workouts focus on strength, muscle endurance, flexibility, stamina & balance. So mix it up, pick formats that help you achieve your personal fitness goals.  You’ll also find unique workout formats designed by Bollywood stars like Hrithik Roshan! We can assure you that your sessions will be more fun, interesting, and less monotonous.  You can find more info about all our workout formats here. If you prefer to workout at home, we've got you covered! You can follow DIY and LIVE videos that are meant for both beginner & advanced levels and focus on building strength, stamina & mobility.";
+    clickforp2++;
+  } else {
+    document.getElementById("p2").innerText = "";
+    clickforp2--;
+  }
+}
+
+function p3() {
+  if (clickforp3 == 0) {
+    document.getElementById("p3").innerText =
+      "With our non-conventional training facility, we aim to give you a balanced mix of workouts that will enhance your health while having fun at the same time. Cult differentiates itself, from other fitness centers by offering group workouts that focus on overall development. Cult has a simple philosophy - make fitness fun and easy with the help of best-in-class trainers and group workouts. Our workouts can be done both at a cult center as group classes and at home with the help of do-it-yourself (DIY) workout videos.  Cult.fit uses the best in technology to give you a world-class experience. You can book classes, follow workout videos - all with the click of a button from the cult.fit app or website.  To know more about cult, check this link. ";
+    clickforp3++;
+  } else {
+    document.getElementById("p3").innerText = "";
+    clickforp3--;
+  }
+}
+
+function p4() {
+  if (clickforp4 == 0) {
+    document.getElementById("p4").innerText =
+      "Cultpass ELITE  is a membership pack where you can book as many classes as you like at any center or gym of your choice in the city. However, you can prebook only 4 classes at any given point in time. The moment you finish one class, you get the option to book another class.  We have 3 month, 6 month & 12 month cultpass. To find out more about memberships & pricing you can check our website.";
+    clickforp4++;
+  } else {
+    document.getElementById("p4").innerText = "";
+    clickforp4--;
+  }
+}
+
+function p5() {
+  if (clickforp5 == 0) {
+    document.getElementById("p5").innerText =
+      "Don’t worry! All our classes are beginner friendly. If you are doing an at Home cult.fit DIY session, we have sessions designed specifically for beginners. At our cult.fit centers too, we welcome beginners and look after them in every class. If it’s your first class, just arrive 10 minutes early, and we’ll make sure we help you get started and feel totally comfortable. To know more about what to expect in your first class read here.";
+    clickforp5++;
+  } else {
+    document.getElementById("p5").innerText = "";
+    clickforp5--;
+  }
+}
+
+function p6() {
+  if (clickforp6 == 0) {
+    document.getElementById("p6").innerText =
+      "cultpass LIVE gives you unlimited access to all LIVE classes and DIY (on demand) sessions. This includes unlimited access to fitness, dance, meditation, health video content and podcasts. A cultpass LIVE member has an all access pass to celebrity masterclasses, ability to workout with friends and track their energy scores, and  monitor their progress in the form of reports. You can purchase the membership through the official cult.fit website or on the android app.";
+    clickforp6++;
+  } else {
+    document.getElementById("p6").innerText = "";
+    clickforp6--;
+  }
+}
+
+function p7() {
+  if (clickforp7 == 0) {
+    document.getElementById("p7").innerText =
+   " Gyms by cult.fit are a collection of handpicked full-service gyms which offer a safe space for you to workout using top quality gym equipment under the guidance of cult certified trainers. At the gyms, you can do a workout of your choice at your convenience. Cult centres offer scheduled trainer led group workouts in different formats like Boxing, HRX, S&C, Yoga and Dance Fitness that focus on overall development. ";
+    clickforp7++;
+  } else {
+    document.getElementById("p7").innerText = "";
+    clickforp7--;
+  }
+}
+
+function p8() {
+  if (clickforp8 == 0) {
+    document.getElementById("p8").innerText =
+      "cultpass ELITE gives you unlimited access to cult group classes, gyms and live workouts. cultpass PRO  gives you unlimited access to gyms and live.workouts and limited access to cult group classes.";
+    clickforp8++;
+  } else {
+    document.getElementById("p8").innerText = "";
+    clickforp8--;
+  }
+}
+
+function p9() {
+  if (clickforp9 == 0) {
+    document.getElementById("p9").innerText =
+      "cult transform connects you with an expert Transform Coach (whatsapp + calls) who guides you through sustainable lifestyle changes to achieve your health goals which include fat loss, weight loss and getting fit. You also get unlimited access to cultpass Live online home workout & meditation sessions, a customised workout plan, nutrition guidance and daily reads on fitness & habits. Please note this does not include a gym membership.";
+    clickforp9++;
+  } else {
+    document.getElementById("p9").innerText = "";
+    clickforp9--;
+  }
+}
+
+function p10() {
+  if (clickforp10 == 0) {
+    document.getElementById("p10").innerText =
+      "There are 3 types of fitness centers available on cultpass. 1) Cult centers. 2) ELITE gyms. 3) PRO gyms. You get unlimited access to all centers in a city with a cultpass ELITE. You get unlimited access to PRO gyms and 4 sessions per month at cult centers or ELITE gyms in a city with cultpass PRO";
+    clickforp10++;
+  } else {
+    document.getElementById("p10").innerText = "";
+    clickforp10--;
+  }
+}
+
+function p11() {
+  if (clickforp11 == 0) {
+    document.getElementById("p11").innerText =
+      "You can access the gyms listed on the cult.fit app by buying a cultpass ELITE or a cultpass PRO. cultpass ELITE will give you unlimited access to all gyms in a city cultpass PRO will give you unlimited access to all PRO gyms and 4 sessions per month at ELITE gyms or cult centers in a city. In addition to gym access, you get unlimited access to LIVE workouts with both the cultpasses";
+    clickforp11++;
+  } else {
+    document.getElementById("p11").innerText = "";
+    clickforp11--;
+  }
+}
+
+function p12() {
+  if (clickforp12 == 0) {
+    document.getElementById("p12").innerText =
+      "PRO gyms are full service gyms that offer you great value. They provide a safe space to workout with top quality equipment and cult certified trainers present to assist you ELITE gyms are a curated collection of handpicked,  full-service gyms at prime locations. They offer everything you get in a PRO gym plus added comfort and luxury to make your workout enjoyable. Lookout for the ELITE tag to to identify a ELITE gym. ";
+    clickforp12++;
+  } else {
+    document.getElementById("p12").innerText = "";
+    clickforp12--;
+  }
+}
